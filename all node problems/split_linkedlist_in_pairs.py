599febfae7d4f7684ce71b95c6cb4e487847ca5b
suhani54 <suhanibachuwar30@gmail.com>
2024-03-27 12:25:33 +0530

    second commit

---
+++ all node problems/split_linkedlist_in_pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        length=0
+        temp=head
+        while temp:
+           length +=1
+           temp=temp.next
+        width,remainder=divmod(length,k)
+
+        result,current=[],head
+        for i in range(k):
+            dummy=write=ListNode(0)
+            for _ in range (width+(i<remainder)) :
+                if current:
+                    write.next=write=ListNode(current.val)
+                    current = current.next
+            result.append(dummy.next)
+        return result
